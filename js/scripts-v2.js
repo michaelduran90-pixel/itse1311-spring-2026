127a90d7b23d3ffa992c5ef66538dc18f8fe2af9
Michael Duran <michael.duran90@gmail.com>
2026-02-02 14:19:25 -0600

    commit

---
+++ js/scripts-v2.js
@@ -0,0 +1,41 @@
+// Data types
+// Boolean: True or False
+// String: Any typical sentence
+// Number: Numeric, Decimal Points
+// Undefined: Does not exist or is not set
+// Null: Non-existant
+
+// var means that the variable has a global scope by default
+// let means that the variable exists and can be changed only inside of its function
+// const means that the variable cannot be changed
+
+// declare means to add a value to a variable or set it empty or null
+
+// to concatenate means to put together, and the symbol is a plus sign +
+
+//var yourName = "Michael Duran";
+
+//const yourName;
+
+
+/*
+function sayMyName() {
+    //some function
+    let yourName = "Michael Duran";
+    yourName = "Michael D. Duran"
+    console.log("My name is " + yourName)
+}
+
+sayMyName();
+*/
+
+
+document.getElementById("myBTN").addEventListener("click", sayMyName);
+
+function sayMyName() {
+    //some function
+    let yourName = document.getElementById("myName").value;
+
+    document.getElementById("myNameDiv").innerHTML = yourName;
+    
+}
---
+++ js/scripts-v3.js
@@ -0,0 +1,43 @@
+console.clear();
+
+// An operand is any object that can be manipulated in an operation (mathematics)
+
+/*
++ addition (concatenate when it is a string)
+++ increment
+- subtraction
+-- decrement
+* multipication
+/ division
+% modulus
+< less than
+> greater than
+<= less than or equals to
+>= greater than or equals to
+= equals (assignation)
+== is equals (comparison)
+=== is identical (comparison)
+!= not equals (comparison)
+!== not identical (comparison)
+&& and
+|| or
+! not 
+*/
+
+let myFirstNumber = 2;
+//let mySecondNumber = 2;
+let mySecondNumber = 3;
+let myThirdNumber = 6;
+
+let myTotal = (myFirstNumber + mySecondNumber * myThirdNumber);
+
+//console.log(typeof myFirstNumber); 
+console.log(myTotal); 
+
+/*
+if (myFirstNumber === mySecondNumber) {
+    console.log("Passes");
+} else{
+    console.log("Not Passes");
+}
+    */
