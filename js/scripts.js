c92e44493365b2acf3b5fcb7553b7f9ca9f465bd
Michael Duran <michael.duran90@gmail.com>
2026-02-03 10:21:51 -0600

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,12 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
+    $('#myBTN').on('click', goToMyFunction);
+
+    function goToMyFunction() {
+        alert('test');
+    }
+
     //$('.myClass').hide();
     $('.myClass').css("color", "purple");
 
