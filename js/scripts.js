48b4121e7b5a355e1bc71dae6a22c1b5e2e5418f
Michael Duran <michael.duran90@gmail.com>
2026-02-03 10:15:27 -0600

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    $('.myClass').hide();
+
+    //$('.myClass').hide();
+    $('.myClass').css("background-color", "purple");
+    
 });
