288ec8de797b3c49069245226c9cf977cd795761
Michael Duran <michael.duran90@gmail.com>
2026-02-12 09:57:01 -0600

    added file

---
+++ php-pages/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$myName = "Michael Duran";
+
+echo "Hello, my name is " . $myName . "<br />";
+
+$firstNum = 6;
+$secondNum = 8;
+
+$total = $firstNum + $secondNum;
+
+echo "my total is " . $total;
