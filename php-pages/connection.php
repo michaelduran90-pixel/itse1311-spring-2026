b61bec3977eb807013eb4904b3ed9310ab727067
Michael Duran <michael.duran90@gmail.com>
2026-02-13 16:38:42 -0600

    commit

--- php-pages/connection.php
+++ php-pages/connection.php
@@ -1,8 +1,9 @@
 <?php
-
+/*
 ini_set('display_error', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
+*/
 
 $server = "localhost";
 $username = "itse1311";
@@ -13,8 +14,8 @@ $conn = new mysqli($server, $username, $password, $database);
 
 //Connection Creation
 if ($conn->connect_error) {
-    die("Connection failed".$conn->connect_error);
+    //die("Connection failed".$conn->connect_error);
 }
 else {
-    echo "Connection Successfull";
+    //echo "Connection Successfull";
 }
