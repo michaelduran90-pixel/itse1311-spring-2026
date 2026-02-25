4b8360d910383d344cdddb05c34c5d1b31a7ab87
Michael Duran <michael.duran90@gmail.com>
2026-02-25 11:07:54 -0600

    commit

--- php-pages/upload.php
+++ php-pages/upload.php
@@ -1,5 +1,31 @@
 <?php
 
 if (isset($_FILE['file'])) {
-    
+
+    $file_name = $_FILE['file']['name'];
+    $file_size = $_FILE['file']['size'];
+    $file_tmp = $_FILE['file']['tmp_name'];
+    $file_type = $_FILE['file']['type'];
+
+    $file_ext = strtolower(end(explode('.', $_FILE['file']['name'])));
+    $extensions = array("pdf", "docx");
+
+    if(in_array($file_ext, $extensions)===false){
+        $errors[] = "Extensions not allowed, please choose a PDF or Word document";
+    }
+
+    if($file_size > 2097152) {
+        $errors[] = "File size must be less than 50MB";
+    }
+
+    if (empty($errors)===TRUE) {
+        move_uploaded_file($file_temp, "upload/".$guid.$file_name);
+    }
+    else{
+        print_r($errors);
+    }
+
+} // is isset
+else {
+    echo "No file uploaded";
 }
