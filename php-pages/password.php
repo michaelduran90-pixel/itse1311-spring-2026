be876eecdb0a177eaf620be776ad0a4674a8ad92
Michael Duran <michael.duran90@gmail.com>
2026-02-27 16:21:02 -0600

    commit

--- php-pages/password.php
+++ php-pages/password.php
@@ -1,6 +1,26 @@
 <?php
 
+$password[] = "password";
+$password[] = "12345";
+$password[] = "000000";
+$password[] = "thisismypassword";
+
 $myPassword = "thisismypassword";
+
+
+if(is_array($password)) {
+
+    foreach($password as $passwords) {
+
+        if($password == $myPassword) {
+            echo "Got hacked";
+        }
+        else {
+            echo "No luck using" . $passwords . "<br />";
+        }
+    }
+
+}//is array
 
 $myPassword = hash('sha256', $myPassword);
 
