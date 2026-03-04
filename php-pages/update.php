89d7b0f91ea789cfb7960b8dc2f5a3799248ae1c
Michael Duran <michael.duran90@gmail.com>
2026-03-04 14:59:10 -0600

    commit

--- php-pages/update.php
+++ php-pages/update.php
@@ -4,9 +4,15 @@ session_start();
 
 include('connection.php');
 
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$age = $_POST['age'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
 $validUser = $_SESSION['validUser'];
 
-$sql = "UPDATE tbl_contacts SET age = '80' WHERE email = '$validUser' ";
+$sql = "UPDATE tbl_contacts SET firstName='$firstName', lastName='$lastName', age='$age', phone='$phone' WHERE email = '$validUser' LIMIT 1";
 
 if ($conn-query($sql)===TRUE) {
     echo "records Updated";
